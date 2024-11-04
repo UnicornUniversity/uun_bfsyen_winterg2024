17d4d826ca684df1b14455a0e67fae91846a6003
IMilota <imilota@centrum.cz>
2024-11-04 12:17:52 +0100

    store today examples

--- client/src/Detail/Item.js
+++ client/src/Detail/Item.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import "./item.css";
 
 function Item({ data, handlerMap }) {
   const [value, setValue] = useState(data.name);
 
   return (
-    <div>
+    <div className={`item ${data.resolved ? "item-resolved" : ""}`}>
       <input
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
         onBlur={() => handlerMap.updateItemName({ id: data.id, name: value })}
       />{" "}
-      <button onClick={() => handlerMap.toggleResolveItem({ id: data.id })}>
+      <Button onClick={() => handlerMap.toggleResolveItem({ id: data.id })}>
         {data.resolved ? "unresolve" : "resolve"}
-      </button>
+      </Button>
       <button onClick={() => handlerMap.deleteItem({ id: data.id })}>
         delete
       </button>
